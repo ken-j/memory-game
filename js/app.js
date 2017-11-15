97596379f05ce6d9333a21ba9961f3d290920f74
ken-j <ken.j.morrissette@gmail.com>
2017-11-15 15:36:36 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ var symbols = ['bicycle', 'bicycle', 'leaf', 'leaf', 'cube', 'cube', 'anchor', '
 		opened = [],
 		match = 0,
 		moves = 0,
+        timeToWin = 0,
+        timer = 0,
         $deck = jQuery('.deck'),
         $winner = $('#winner'),
 		$scorePanel = $('#score-panel'),
@@ -13,6 +15,11 @@ var symbols = ['bicycle', 'bicycle', 'leaf', 'leaf', 'cube', 'cube', 'anchor', '
 		rank3stars = gameCardsQTY + 2,
 		rank2stars = gameCardsQTY + 6,
 		rank1stars = gameCardsQTY + 10;
+
+function gameTimer() {
+    timeToWin++;
+    document.getElementById("gameTimer").innerHTML = timeToWin;
+}
 
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
@@ -28,12 +35,14 @@ function shuffle(array) {
   return array;
 }
 
+// Initial Game
 function initGame() {
-    console.log('game initGame');
   var cards = shuffle(symbols);
   $deck.empty();
   match = 0;
   moves = 0;
+  timeToWin = -1;
+  timer = setInterval(gameTimer, 1000);
   $moveNum.text('0');
   $ratingStars.removeClass('fa-star-o').addClass('fa-star');
 	for (var i = 0; i < cards.length; i++) {
@@ -42,31 +51,44 @@ function initGame() {
 	addCardListener();
 };
 
+// Set Rating and final Score
 function setRating(moves) {
 	var rating = 3;
-	if (moves > rank3stars && moves < rank2stars) {
+	if (moves > rank3stars && moves <= rank2stars) {
 		$ratingStars.eq(2).removeClass('fa-star').addClass('fa-star-o');
 		rating = 2;
-	} else if (moves > rank2stars && moves < rank1stars) {
+	} else if (moves > rank2stars && moves <= rank1stars) {
 		$ratingStars.eq(1).removeClass('fa-star').addClass('fa-star-o');
 		rating = 1;
-	} else if (moves > rank1stars) {
-		$ratingStars.eq(0).removeClass('fa-star').addClass('fa-star-o');
-		rating = 0;
-	}	
+    } else if (moves > rank1stars) {
+		rating = 1;
+	}
 	return { score: rating };
 };
 
+// End Game
 function endGame(moves, score) {
-    $winner.append($('<p>Winner, With ' + moves + ' Moves and ' + score + ' Stars.</p>'))
+    $.sweetModal({
+        title: 'You Won!',
+        content: '<p>Winner, With ' + timeToWin + ' Seconds and ' + score + ' Stars.</p>',
+       buttons: [
+		{
+			label: 'Restart',
+			classes: 'greenB',
+            action: function() {
+				return initGame();
+			}
+		}
+	]
+    });
 }
 
-$restart.bind('click', function() {
+// Restart Game
+$restart.on('click', function() {
   initGame();
 });
 
 var addCardListener = function() {
-console.log('addCardListener');
 // Card flip
 $deck.find('.card:not(".match, .open")').bind('click' , function() {
 	if($('.show').length > 1) { return true; }
@@ -78,6 +100,7 @@ $deck.find('.card:not(".match, .open")').bind('click' , function() {
   $this.addClass('open show');
 	opened.push(card);
 
+	// Compare with opened card
   if (opened.length > 1) {
     if (card === opened[0]) {
       $deck.find('.open').addClass('match animated infinite rubberBand');
@@ -100,10 +123,11 @@ $deck.find('.card:not(".match, .open")').bind('click' , function() {
 		$moveNum.html(moves);
   }
 	
-	
+	// End Game if match all cards
 	if (gameCardsQTY === match) {
 		setRating(moves);
 		var score = setRating(moves).score;
+        clearInterval(timer);
 		setTimeout(function() {
 			endGame(moves, score);
 		}, 500);
